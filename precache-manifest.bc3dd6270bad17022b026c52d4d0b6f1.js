0b90c8c09fa5f4567cddcd9a9ce15393fd95a71f
unknown <shklyarigor2015@gmail.com>
2019-09-11 13:20:05 +0300

    Updates

---
+++ precache-manifest.bc3dd6270bad17022b026c52d4d0b6f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c60916b69b0cf4af67e6d6270e23b00",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "28a42344ccfbbd7e26f9",
+    "url": "/react_static-list-of-posts/static/css/main.b5aa5b44.chunk.css"
+  },
+  {
+    "revision": "893a8acb1a0ad8980363",
+    "url": "/react_static-list-of-posts/static/js/2.f00605ee.chunk.js"
+  },
+  {
+    "revision": "28a42344ccfbbd7e26f9",
+    "url": "/react_static-list-of-posts/static/js/main.266d461f.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
